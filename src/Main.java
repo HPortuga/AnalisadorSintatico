0a306bc469612b871c8f54528096c61ef41c6ff4
Matheus Henrique <mathsoares1@gmail.com>
2019-05-24 08:39:06 -0400

    Adicionado leitura de arquivo

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,33 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
+        String input = lerArquivo("code.xpp");
 
-        System.out.println("Hello World!");
+    }
+
+    private static String lerArquivo(String fileName) {
+        String input = "";
+        FileReader fileReader = null;
+        try {
+            fileReader = new FileReader(fileName);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try (BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                input += (line + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return input;
     }
 }
