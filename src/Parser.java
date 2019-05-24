9a411e6fc471dd1112c21075cbb310b181f94a66
Matheus Henrique <mathsoares1@gmail.com>
2019-05-24 09:04:29 -0400

    Adicionado CompilerException e métodos de teste do parser

--- src/Parser.java
+++ src/Parser.java
@@ -1,2 +1,17 @@
 public class Parser {
+   private Token token;
+   private Scanner scanner;
+
+   public Parser(String input) {
+      this.scanner = new Scanner(input);
+      advance();
+   }
+
+   private void advance() {
+      this.token = this.scanner.nextToken();
+   }
+
+   public void program() {
+
+   }
 }
---
+++ src/ParserTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+
+class ScannerTest {
+
+   @Test
+   void deve_identificar_Factor() {
+
+   }
+}
